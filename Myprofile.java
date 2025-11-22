e3915e0f26cd7e55413ba0cdf210f3cb5056a599
Shubham Sedai <subhamreader@gmail.com>
2025-11-22 20:16:42 +0545

    Delete Myprofile.java

--- Myprofile.java
+++
@@ -1,8 +0,0 @@
-class Myprofile {
-    public static void main(String[] args){ 
-  System.out.println("my name is shubham"); 
-  System.out.println("my favorite hobby is videogame");
-  System.out.println("i chose to be a programmer");
-  
-}
- }   
